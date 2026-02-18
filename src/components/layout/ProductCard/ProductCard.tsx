bfd969e6c914672132dbb5763d9e8fb9c81881be
Roman Martseniuk <martseniuk.roma@gmail.com>
2026-02-18 12:24:44 +0200

    add loader to carousel in `Product Card`

--- src/components/layout/ProductCard/ProductCard.tsx
+++ src/components/layout/ProductCard/ProductCard.tsx
@@ -1,6 +1,12 @@
 import CloseIcon from "@/assets/icons/CloseIcon";
 import { Badge } from "@/components/ui/badge";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 import {
   DialogClose,
@@ -11,6 +17,8 @@ import {
 import type { Product } from "@/types/Product";
 
 import DOMPurify from "dompurify";
+import { useState } from "react";
+import { Loader } from "../Loader";
 
 const BASE_URL = "https://kiborg.com.ua";
 
@@ -35,7 +43,6 @@ function fixImages(html: string) {
     wrapper.appendChild(table);
   });
 
-
   return doc.body.innerHTML;
 }
 
@@ -45,6 +52,8 @@ export const ProductCard = ({ product }: { product: Product }) => {
   });
 
   const cleanHtml = fixImages(sanitized);
+
+  const [isImgLoaded, setIsImgLoaded] = useState(false);
 
   return (
     <DialogContent
@@ -60,44 +69,51 @@ export const ProductCard = ({ product }: { product: Product }) => {
         </DialogClose>
       </DialogHeader>
       <div className="no-scrollbar max-h-[70vh] px-6 mb-6 overflow-y-auto">
-        <div className="relative mb-5 pt-5 flex flex-col">
-          <Carousel
-            className="relative"
-            opts={{
-              loop: true,
-            }}
-            plugins={[
-              Autoplay({
-                delay: 3000,
-              }),
-            ]}
-          >
-            <CarouselContent>
-              {product.pictures.map((pic) => (
-                <CarouselItem key={pic}>
-                  <img
-                    src={pic}
-                    alt={product.name}
-                    className="mb-2  md:w-80 aspect-square object-contain m-auto"
-                  />
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious className="absolute left-0 bg-[#8f9250] hover:bg-[#6e6e49] border-0" />
-            <CarouselNext className="absolute right-0 bg-[#8f9250] hover:bg-[#6e6e49] border-0" />
-          </Carousel>
+        <div className="relative mb-2 pt-5 flex flex-col">
+          {/* Loader поверх */}
+          {!isImgLoaded && (
+            <div className="absolute inset-0 flex items-center justify-center z-10 bg-white">
+              <Loader className="h-12 w-12" />
+            </div>
+          )}
 
-          <div className="flex gap-2 flex-wrap items-center">
-            <Badge className="bg-[#8F9250]">РРЦ: {product.price} грн</Badge>
-            {product.optPrice && (
-              <Badge className="bg-[#6E6E49]">
-                ОПТ: {product.optPrice} грн
-              </Badge>
-            )}
-            <Badge className="bg-[#CC8110]">
-              ДРОП: {product.dropPrice} грн
-            </Badge>
+          <div
+            className={
+              isImgLoaded
+                ? "opacity-100 transition-opacity duration-300"
+                : "opacity-0"
+            }
+          >
+            <Carousel
+              className="relative"
+              opts={{ loop: true }}
+              plugins={[Autoplay({ delay: 3000 })]}
+            >
+              <CarouselContent>
+                {product.pictures.map((pic) => (
+                  <CarouselItem key={pic}>
+                    <img
+                      src={pic}
+                      alt={product.name}
+                      className="md:w-80 aspect-square object-contain m-auto"
+                      onLoad={() => setIsImgLoaded(true)}
+                    />
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+
+              <CarouselPrevious className="absolute left-0 bg-[#8f9250] hover:bg-[#6e6e49] border-0" />
+              <CarouselNext className="absolute right-0 bg-[#8f9250] hover:bg-[#6e6e49] border-0" />
+            </Carousel>
           </div>
+        </div>
+
+        <div className="flex gap-2 flex-wrap items-center mb-5">
+          <Badge className="bg-[#8F9250]">РРЦ: {product.price} грн</Badge>
+          {product.optPrice && (
+            <Badge className="bg-[#6E6E49]">ОПТ: {product.optPrice} грн</Badge>
+          )}
+          <Badge className="bg-[#CC8110]">ДРОП: {product.dropPrice} грн</Badge>
         </div>
         <div className="flex items-center justify-between mb-4">
           <div className="text-[12px]">Артикул - {product.vendorCode}</div>
