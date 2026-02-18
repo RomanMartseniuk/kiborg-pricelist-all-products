1dba174765dc2bdb92db37aaff0bcd7a1f8d11d0
Roman Martseniuk <martseniuk.roma@gmail.com>
2026-02-18 14:44:25 +0200

    fix prices layout

--- src/components/layout/TableItem/TableItem.tsx
+++ src/components/layout/TableItem/TableItem.tsx
@@ -22,15 +22,15 @@ const TableItem: React.FC<Props> = ({ product }) => {
             {product.name}
           </TableCell>
 
-          <TableCell className="border border-border font-[Unbounded] text-center text-[7px] sm:text-[14px]">
+          <TableCell className="m-0! p-0! border border-border font-[Unbounded] text-center text-[7px] sm:text-[14px]">
             {Math.round(product.price)}
           </TableCell>
 
-          <TableCell className="border border-border font-[Unbounded] text-center text-[7px] sm:text-[14px]">
+          <TableCell className="m-0! p-0! border border-border font-[Unbounded] text-center text-[7px] sm:text-[14px]">
             {product.optPrice ? Math.round(product.optPrice) : "-"}
           </TableCell>
 
-          <TableCell className="border border-border font-[Unbounded] text-center text-[7px] sm:text-[14px]">
+          <TableCell className="m-0! p-0! border border-border font-[Unbounded] text-center text-[7px] sm:text-[14px]">
             {Math.round(product.dropPrice)}
           </TableCell>
         </TableRow>
